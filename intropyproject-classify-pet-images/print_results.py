07fd91339876ea4b49ab7f7a859b752c64f73846
nvin00924 <nvin0924@gmail.com>
2026-08-20 10:49:25 -0400

    Update print_results.py
    TODO 6

--- intropyproject-classify-pet-images/print_results.py
+++ intropyproject-classify-pet-images/print_results.py
@@ -61,6 +61,34 @@ def print_results(results_dic, results_stats_dic, model,
                               False doesn't print anything(default) (bool) 
     Returns:
            None - simply printing results.
-    """    
-    None
+    """ 
+      print("Results Summary for CNN Model Architecture:", model.upper()) 
+      print("{:<20}: {:>3}".format('N Images', results_stats_dic['n_images']))
+      print("{:<20}: {:>3}".format('N Dog Images', results_stats_dic['n_dogs_img']))
+      print("{:<20}: {:>3}".format('N Not-Dog Images', results_stats_dic['n_notdogs_img']))
+
+      print("\nSummary Statistics (Percentages):")
+      for key in results_stats_dic:
+        if key.startswith('p'):
+            print("{:<25}: {:.2f}%".format(key, results_stats_dic[key]))
+            
+        if (print_incorrect_dogs and 
+          (results_stats_dic['n_correct_dogs'] + results_stats_dic['n_correct_notdogs'] != results_stats_dic['n_images'])):
+          print("\nINCORRECT Dog Classification:")
+
+          for key in results_dic:
+              if sum(results_dic[key][3:]) == 1:
+                  print("Real Label: {:>26}   Classifier Label: {:>30}".format(
+                      results_dic[key][0], results_dic[key][1]))
+
+        if (print_incorrect_breed and 
+          (results_stats_dic['n_correct_dogs'] != results_stats_dic['n_correct_breed'])):
+          print("\nINCORRECT Dog Breed Classification:")
+
+          for key in results_dic:
+            if sum(results_dic[key][3:]) == 2 and results_dic[key][2] == 0:
+                print("Real Label: {:>26}   Classifier Label: {:>30}".format(
+                    results_dic[key][0], results_dic[key][1]))
+
+    
                 
